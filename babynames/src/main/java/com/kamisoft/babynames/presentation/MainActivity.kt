2518243cb5475c01c1e0bc970ad995fdbf938d99
Pabloku <paloga15@hotmail.com>
2017-04-23 14:10:04 +0200

    MainACtivity changes - Added a stepped indicator from com.github.badoualy:stepper-indicator library - build.gradle and dependencies.gradle updated to use com.github.badoualy:stepper-indicator

--- babynames/src/main/java/com/kamisoft/babynames/presentation/MainActivity.kt
+++ babynames/src/main/java/com/kamisoft/babynames/presentation/MainActivity.kt
@@ -3,31 +3,15 @@ package com.kamisoft.babynames.presentation
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.kamisoft.babyname.R
-import com.kamisoft.babynames.data.datasource.NamesDataFactory
-import com.kamisoft.babynames.data.datasource.NamesDataSource
-import com.kamisoft.babynames.data.repository.NamesDataRepository
-import com.kamisoft.babynames.domain.usecase.GetNameList
-import com.kamisoft.babynames.logger.Logger
+import com.kamisoft.babynames.presentation.chooseGenre.ChooseGenreFragment
 
-class MainActivity : AppCompatActivity(), GetNameList.CallBacks.NamesCallback {
-    override fun namesLoaded(nameList: List<String>) {
-        nameList.forEach { Logger.info(it) }
-        Logger.info("Process end")
-    }
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Logger.start()
 
-        /*doAsync {
-            Logger.info("Process start")
-            val names = Tasks.await(FirebaseNamesDataSource().getNameListTask(NamesDataSource.Genre.MALE))
-            names.forEach { Logger.info(it) }
-            Logger.info("Process end")
-        }*/
-
-        Logger.info("Process start")
-        GetNameList(NamesDataRepository(NamesDataFactory())).getNames(NamesDataSource.Genre.FEMALE, this)
+        val transition = supportFragmentManager.beginTransaction()
+        transition.replace(R.id.contentView, ChooseGenreFragment.createInstance())
+        transition.commit()
     }
 }
