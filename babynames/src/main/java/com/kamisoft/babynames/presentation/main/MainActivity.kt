9722a424c48b54077d64c08380101e2309f9fe82
Pabloku <paloga15@hotmail.com>
2017-05-01 16:23:07 +0200

    New MatchesFragment - Added new strings - MatchesFragment included in the MainActivity nagigation logic

--- babynames/src/main/java/com/kamisoft/babynames/presentation/main/MainActivity.kt
+++ babynames/src/main/java/com/kamisoft/babynames/presentation/main/MainActivity.kt
@@ -6,16 +6,23 @@ import com.kamisoft.babyname.R
 import com.kamisoft.babynames.data.datasource.NamesDataSource
 import com.kamisoft.babynames.domain.model.BabyName
 import com.kamisoft.babynames.domain.model.Parent
+import com.kamisoft.babynames.logger.Logger
 import com.kamisoft.babynames.presentation.choose_gender.ChooseGenderFragment
 import com.kamisoft.babynames.presentation.choose_name.ChooseNameFragment
+import com.kamisoft.babynames.presentation.matches.MatchesFragment
 import com.kamisoft.babynames.presentation.who_choose.WhoChooseFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(),
         ChooseGenderFragment.ChooseGenderListener,
         WhoChooseFragment.WhoChooseListener,
-        ChooseNameFragment.ChooseNamesListener {
-    lateinit var gender: NamesDataSource.Gender
+        ChooseNameFragment.ChooseNamesListener,
+        MatchesFragment.MatchesListener {
+
+    private lateinit var gender: NamesDataSource.Gender
+
+    private lateinit var babyNamesFirstParent: List<BabyName>
+    private lateinit var babyNamesSecondParent: List<BabyName>
 
     override fun onGenderSelected(gender: NamesDataSource.Gender) {
         this.gender = gender
@@ -29,11 +36,23 @@ class MainActivity : AppCompatActivity(),
     override fun onNamesSelected(babyNamesLiked: List<BabyName>, parentPosition: Int) {
         //TODO save babyNamesLiked in memory
         if (parentPosition == 1) {
+            babyNamesFirstParent = babyNamesLiked
             showWhoChooseView(2)
         } else {
             //TODO show coincidences fragment!
+            babyNamesSecondParent = babyNamesLiked
+            val list = babyNamesFirstParent.filter {
+                val name1 = it.name
+                val babyName2 = babyNamesSecondParent.find { it.name == name1 }
+                return@filter babyName2 != null
+            }
+            showCoincidencesView(ArrayList(list.map { it.name }))
         }
     }
+    override fun onAccept() {
+        Logger.debug("Matches acepted")
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -76,6 +95,15 @@ class MainActivity : AppCompatActivity(),
             stepperIndicator.currentStep = 4
             transaction.addToBackStack("chooseNameSecondParent")
         }
+        transaction.commit()
+    }
+
+    fun showCoincidencesView(matches: ArrayList<String>) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left,
+                R.anim.enter_from_left, R.anim.exit_to_right);
+        transaction.replace(R.id.contentView, MatchesFragment.createInstance(matches))
+        transaction.addToBackStack("coincidences")
         transaction.commit()
     }
 
---
+++ babynames/src/main/java/com/kamisoft/babynames/presentation/matches/MatchesFragment.kt
@@ -0,0 +1,70 @@
+package com.kamisoft.babynames.presentation.matches
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kamisoft.babyname.R
+import com.kamisoft.babynames.domain.model.BabyName
+import com.kamisoft.babynames.logger.Logger
+import com.kamisoft.babynames.presentation.choose_name.adapter.NameItemAnimator
+import com.kamisoft.babynames.presentation.choose_name.adapter.NamesAdapter
+import kotlinx.android.synthetic.main.fragment_choose_name.*
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+class MatchesFragment : Fragment() {
+
+    interface MatchesListener {
+        fun onAccept()
+    }
+
+    var callBack: MatchesListener? = null
+
+    companion object {
+        const val ARG_MATCH_LIST = "matchList"
+        fun createInstance(matches: ArrayList<String>): MatchesFragment {
+            val fragment = MatchesFragment()
+            val bundle = Bundle()
+            bundle.putStringArrayList(ARG_MATCH_LIST, matches)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
+    private val nameMatchesList: ArrayList<String> by MatchesArgument(ARG_MATCH_LIST)
+
+    private val namesAdapter: NamesAdapter = NamesAdapter {
+        Logger.debug("${it.name} Clicked")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_matches, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rvList.layoutManager = LinearLayoutManager(activity)
+        rvList.adapter = namesAdapter
+        rvList.itemAnimator = NameItemAnimator()
+        namesAdapter.setBabyNameList(nameMatchesList.map { BabyName(it, false) })
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+
+        if (context !is MatchesListener) {
+            throw IllegalStateException("The attaching activity has to implement ${MatchesListener::class.java.canonicalName}")
+        }
+        callBack = context
+    }
+
+    class MatchesArgument(private val arg: String) : ReadOnlyProperty<Fragment, ArrayList<String>> {
+        override fun getValue(thisRef: Fragment, property: KProperty<*>): ArrayList<String> {
+            return thisRef.arguments.getStringArrayList(arg)
+        }
+    }
+}
